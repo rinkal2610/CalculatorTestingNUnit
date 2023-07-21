b04791c3d065024bba9c9451a0f9c062ead7f5ee
rinkal2610 <rinkalpatel2233@gmail.com>
2023-07-21 16:20:15 +0530

    initial commit

---
+++ CalcNunitEx1Day10/Calc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcNunitEx1Day10
+{
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+        static int x= 2;
+        public bool IsPrime(int a)
+        {
+            if (a==0 || a==1) return false;
+            if(a==x) return true;
+            if(a%x==0) return false;
+            x++;
+            return IsPrime(a);
+        }
+      
+    }
+}
---
+++ CalcNunitEx1Day10/CalcTestClass.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcNunitEx1Day10
+{
+    [TestFixture]
+    public class CalcTestClass
+    {
+        Calc calc=new Calc();
+        [Test]
+        public void TestAdd()
+        {
+            Assert.AreEqual(10, calc.Add(6, 4));
+        }
+        [Test]
+        public void TestSub()
+        {
+            Assert.AreEqual(2, calc.Sub(6, 4));
+        }
+        [Test]
+        public void TestMulti()
+        {
+            Assert.AreEqual(24, calc.Multi(6, 4));
+        }
+        [Test]
+        public void TestDiv()
+        {
+            Assert.AreEqual(4, calc.Div(16, 4));
+        }
+        [Test]
+        public void TestPrime()
+        {
+            Assert.AreEqual(true, calc.IsPrime(5));
+        }
+    }
+}
